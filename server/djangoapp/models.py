14e38237848193d7a6dc3d0a57318a4fc3708b8f
Mark Montero <markc.montero@gmail.com>
2022-05-17 02:24:59 +0000

    Added Django Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,16 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=100)
+    country = models.CharField(null=False, max_length=30)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " + self.name + " " +  \
+            "Description: " + self.description+ " " +  \
+            "Country: " + self.country
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,7 +29,25 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, null=False, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    dealer_id = models.IntegerField(null=False)
+    type = models.CharField(null=False, max_length=30, choices=[('Sedan', 'Sedan'), ('SUV', 'SUV'), ('WAGON','WAGON')])
+    year = models.DateField(null=True)
+    length = models.IntegerField(null=True)
+    width = models.IntegerField(null=True)
+    height = models.IntegerField(null=True)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " +self.name + " " + \
+            "Dealer ID: " + str(self.dealer_id) + " " +  \
+            "Type: " + self.type + " " +  \
+            "Year: " + str(self.year) + " " +  \
+            "Length: " + str(self.length) + " " +  \
+            "Width: " + str(self.width) + " " +  \
+            "Height: " + str(self.height)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
